17e112daec07eefedafe339ff0fc5002ac556a80
Matthew Kingham <matthew.kingham@openecx.co.uk>
2020-05-24 14:33:19 +0100

    Update

---
+++ build/precache-manifest.b139f81c76575d3b98a046ec4d739356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e724b88facdd2eb41888f16241b591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33a65ae6b47ca8f07cd6",
+    "url": "/static/css/main.9974a49b.chunk.css"
+  },
+  {
+    "revision": "73a6d3ab5fd2d58f265a",
+    "url": "/static/js/2.a97d2f48.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.a97d2f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a65ae6b47ca8f07cd6",
+    "url": "/static/js/main.e79b36be.chunk.js"
+  },
+  {
+    "revision": "2cd0a6d9d93612e2ef58",
+    "url": "/static/js/runtime-main.92282b2d.js"
+  }
+]);
